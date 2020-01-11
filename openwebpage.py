b8bf79b89bd8fd9951c68b11b2c09490e8468777
Fr0st-root <59753329+Fr0st-root@users.noreply.github.com>
2020-01-10 22:26:55 -0500

    Update openwebpage.py

--- openwebpage.py
+++ openwebpage.py
@@ -3,4 +3,4 @@ import webbrowser
 
 print('Hello discord! Today we are going to open a webbrowser from a remote server using a backdoor.')
 site = 'https://www.tedcruzforhumanpresident.com/'
-webbrowser.get('chrome').open_new(site)
+webbrowser.open(site)
